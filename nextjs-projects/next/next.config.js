94f2c10ef334783424497ae72295051781139903
Christopher Pezza <chris.pezza@hey.com>
2023-03-25 13:02:46 -0400

    add next files for nextjs

---
+++ nextjs-projects/next/next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+module.exports = {
+  reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    loader: 'akamai',
+    path: '/',
+  },
+}
